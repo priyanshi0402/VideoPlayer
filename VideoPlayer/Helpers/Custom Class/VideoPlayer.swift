99ca805e45766392e80525b3b04cd53960d92656
Priyanshi Bhikadiya <98273222+priyanshi0402@users.noreply.github.com>
2023-10-25 09:09:32 +0530

    First Commit

---
+++ VideoPlayer/Helpers/Custom Class/VideoPlayer.swift
@@ -0,0 +1,246 @@
+//
+//  VideoPlayer.swift
+//  VideoPlayer
+//
+//  Created by Tecocraft on 23/10/23.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class VideoPlayer: UIView {
+    
+    private let videoPlayerLayer: AVPlayerLayer = AVPlayerLayer()
+    private var player = AVPlayer()
+    private var timeObserver: AnyObject?
+    
+    public var status: PlayerStatus = .new
+    public var playingVideo: ((_ progress: Double) -> Void)?
+    public var startedVideo: (() -> Void)?
+    public var stoppedVideo: (() -> Void)?
+    public var pausedVideo: (() -> Void)?
+    
+    fileprivate(set) var progress: Double = 0.0
+    @objc internal var isInteracting: Bool = false
+    
+    open var videoLength: Double {
+        if let duration = player.currentItem?.asset.duration {
+            return duration.seconds
+        }
+        return 0.0
+    }
+    
+    open var currentTime: Double {
+        if let time = player.currentItem?.currentTime() {
+            return time.seconds
+        }
+
+        return 0.0
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+        
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        self.videoPlayerLayer.frame = self.bounds
+        layer.addSublayer(self.videoPlayerLayer)
+        videoPlayerLayer.player = player
+        videoPlayerLayer.contentsScale = UIScreen.main.scale
+    }
+    
+    func setupVideoPlayer(url: URL) {
+        let asset = AVAsset(url: url)
+        setVideoAsset(asset: asset)
+    }
+    
+    private func setVideoAsset(asset: AVAsset) {
+        videoPlayerLayer.videoGravity = .resizeAspect
+        self.switchTrack(index: 0, isFirstTime: true)
+    }
+    
+    fileprivate func deinitObservers() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+        if let video = player.currentItem, video.observationInfo != nil {
+            video.removeObserver(self, forKeyPath: "status")
+        }
+        
+        if let observer = timeObserver {
+            player.removeTimeObserver(observer)
+            timeObserver = nil
+        }
+    }
+    
+    open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        guard let asset = object as? AVPlayerItem, let keyPath = keyPath else { return }
+
+        if asset == player.currentItem && keyPath == "status" {
+            if asset.status == .readyToPlay {
+                if status == .new {
+                    status = .readyToPlay
+                }
+                addTimeObserver()
+                playVideo()
+            }
+        }
+    }
+    
+    fileprivate func addTimeObserver() {
+        if let observer = timeObserver {
+            player.removeTimeObserver(observer)
+        }
+        
+        timeObserver = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.01, preferredTimescale: Int32(NSEC_PER_SEC)), queue: nil, using: { [weak self] (time) in
+            guard let strongSelf = self/*, strongSelf.status == .playing */else { return }
+            
+            let currentTime = time.seconds
+            strongSelf.progress = currentTime / (strongSelf.videoLength != 0.0 ? strongSelf.videoLength : 1.0)
+            strongSelf.playingVideo?(strongSelf.progress)
+            //            strongSelf.playingVideo?(strongSelf.progress)
+            //            strongSelf.delegate?.playingVideo?(progress: strongSelf.progress)
+        }) as AnyObject?
+    }
+    
+    @objc internal func itemDidFinishPlaying(_ notification: Notification) {
+        stopVideo()
+    }
+}
+
+// MARK: - Videoplayer actions
+extension VideoPlayer {
+    
+    func switchTrack(index: Int, isFirstTime: Bool = false) {
+        deinitObservers()
+        guard let url = Bundle.main.url(forResource: "test_video", withExtension: "mp4") else { return }
+        let asset = AVAsset(url: url)
+        let composition = AVMutableComposition()
+        
+        let currentTime = self.player.currentTime()
+        let videoTrack = asset.tracks(withMediaType: .video)[index]
+        
+        let compositionTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        do {
+            try compositionTrack?.insertTimeRange(CMTimeRange(start: .zero, duration: asset.duration), of: videoTrack, at: .zero)
+        } catch {
+            print(error.localizedDescription)
+        }
+        let playerItem = AVPlayerItem(asset: composition)
+        // Replace the current player item with the new player item
+        player.replaceCurrentItem(with: playerItem)
+        if !isFirstTime {
+            player.seek(to: currentTime, toleranceBefore: .zero, toleranceAfter: .zero)
+        }
+        
+        player.play()
+        player.currentItem?.addObserver(self, forKeyPath: "status", options: [], context: nil)
+
+    }
+    
+    public func seek(min: Double = 0.0, max: Double = 1.0, value: Double) {
+        let value = rangeMap(value, min: min, max: max, newMin: 0.0, newMax: 1.0)
+        self.seek(value)
+    }
+    
+    private func seek(_ percentage: Double) {
+        progress = min(1.0, max(0.0, percentage))
+        guard let currentItem = player.currentItem else { return }
+
+        if progress == 0.0 {
+            seekToZero()
+            playingVideo?(progress)
+//            delegate?.playingVideo?(progress: progress)
+        } else {
+            let time = CMTime(seconds: progress * currentItem.asset.duration.seconds, preferredTimescale: currentItem.asset.duration.timescale)
+            player.seek(to: time, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero, completionHandler: { (finished) in
+//                guard let strongSelf = self else { return }
+//                if finished == false {
+////                    strongSelf.seekStarted?()
+////                    strongSelf.delegate?.seekStarted?()
+//                } else {
+//                    strongSelf.stoppedVideo?()
+////                    strongSelf.seekEnded?()
+////                    strongSelf.playingVideo?(strongSelf.progress)
+////                    strongSelf.delegate?.seekEnded?()
+////                    strongSelf.delegate?.playingVideo?(progress: strongSelf.progress)
+//                }
+            })
+        }
+    }
+    
+    
+    fileprivate func seekToZero() {
+        progress = 0.0
+        let time = CMTime(seconds: 0.0, preferredTimescale: 1)
+        player.seek(to: time, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
+    }
+    
+    @objc open func playVideo() {
+        guard let playerItem = player.currentItem else { return }
+
+        if progress >= 1.0 {
+            seekToZero()
+        }
+        status = .playing
+        self.player.play()
+        startedVideo?()
+
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(itemDidFinishPlaying(_:)) , name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: playerItem)
+    }
+    
+    public func stopVideo() {
+        player.rate = 0.0
+        seekToZero()
+        status = .stopped
+        stoppedVideo?()
+    }
+    
+    public func pauseVideo() {
+        player.rate = 0.0
+        status = .paused
+        pausedVideo?()
+    }
+    
+    public func toggleVideoPlay() {
+        if self.status == .playing {
+            pauseVideo()
+        } else {
+            playVideo()
+        }
+    }
+    
+    public func forwardVideo() {
+        let moveForword : Float64 = 5.0
+        let newTime = self.currentTime + moveForword
+        if newTime < self.videoLength {
+            let selectedTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+            player.seek(to: selectedTime)
+        } else {
+            let selectedTime: CMTime = CMTimeMake(value: Int64(self.videoLength * 1000 as Float64), timescale: 1000)
+            player.seek(to: selectedTime)
+        }
+        player.pause()
+        player.play()
+    }
+    
+    public func backwardVideo() {
+        let moveBackword: Float64 = 5.0
+  
+        var newTime = self.currentTime - moveBackword
+        if newTime < 0 {
+            newTime = 0
+        }
+        player.pause()
+        let selectedTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+        player.seek(to: selectedTime)
+        player.play()
+    }
+}
--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -6,14 +6,168 @@
 //
 
 import UIKit
+import AVKit
+import Foundation
+
+public enum PlayerStatus {
+    case new /// A new video has been assigned.
+    case readyToPlay /// The video is ready to be played.
+    case playing /// The video is currently being played.
+    case paused /// The video has been paused.
+    case stopped /// The video playback has been stopped.
+    case error /// An error occured. For more details use the `error` closure.
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPlayPause: UIButton!
+    @IBOutlet weak var videoContainerView: VideoPlayer!
+    @IBOutlet weak var safeAreaBottomView: UIView!
+    @IBOutlet weak var dropDownMenu: DropDown!
+    @IBOutlet weak var progressSlider: Scrubber!
+    @IBOutlet weak var lblEndTime: UILabel!
+    @IBOutlet weak var lblStartTime: UILabel!
+    @IBOutlet weak var bottomControlView: UIView!
+    
+    private var controlsToggleWorkItem: DispatchWorkItem?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setUpDropDownMenu()
+        
+        self.bottomControlView.backgroundColor = .white.withAlphaComponent(0.75)
+        self.progressSlider.tintColor = .blue
+        self.progressSlider.trackColor = UIColor.white.cgColor
+        
+        progressSlider.addTarget(self, action: #selector(ViewController.progressSliderChanged(slider:)), for: [.touchUpInside])
+        progressSlider.addTarget(self, action: #selector(ViewController.progressSliderBeginTouch), for: [.touchDown])
+        self.setUpVideoView()
+    }
+    
+    private func setUpVideoView() {
+        
+        guard let url = Bundle.main.url(forResource: "test_video", withExtension: "mp4") else { return }
+//        self.videoContainerView.videoUrl = url
+        videoContainerView.setupVideoPlayer(url: url)
+        lblStartTime.text = self.timeFormatted(totalSeconds: 0)
+        lblEndTime.text = self.timeFormatted(totalSeconds: UInt(self.videoContainerView.videoLength))
+        self.videoContainerView.playingVideo = { [weak self, weak videoContainerView] progress in
+            guard let strongSelf = self, let strongVideoPlayerView = videoContainerView else { return }
+            strongSelf.progressSlider.value = progress
+            let currentTime = strongVideoPlayerView.currentTime
+            strongSelf.lblStartTime.text = strongSelf.timeFormatted(totalSeconds: UInt(currentTime))
+        }
+        
+        self.videoContainerView.stoppedVideo = { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.btnPlayPause.setImage(.icPlayVideo, for: .normal)
+        }
+        
+    }
+    
+    private func setUpDropDownMenu() {
+        self.dropDownMenu.optionArray = ["Front", "Rear"]
+        self.dropDownMenu.isSearchEnable = false
+        
+        self.dropDownMenu.arrowSize = 15
+        self.dropDownMenu.selectedRowColor = .clear
+        self.dropDownMenu.cornerRadius = 20
+        self.dropDownMenu.rowHeight = 40
+        self.dropDownMenu.text = "Front"
+        self.dropDownMenu.font = .systemFont(ofSize: 16)
+        
+        self.dropDownMenu.didSelect { selectedText, index, id in
+            self.videoContainerView.switchTrack(index: index)
+        }
+    }
+
+    @IBAction func btnBackwardVideoAction(_ sender: Any) {
+        self.videoContainerView.backwardVideo()
+    }
+    
+    @IBAction func btnPreviousVideoAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnForwardVideoAction(_ sender: Any) {
+        self.videoContainerView.forwardVideo()
+    }
+    
+    @IBAction func btnNextVideoAction(_ sender: Any) {
+    }
+    
+    @IBAction func btnPlayPauseToggleAction(_ sender: UIButton) {
+        self.videoContainerView.toggleVideoPlay()
+        let image: UIImage = self.videoContainerView.status == .playing ? .icPauseVideo : .icPlayVideo
+        sender.setImage(image, for: .normal)
+    }
+    
+    @objc internal func progressSliderChanged(slider: Scrubber) {
+        print("progressSliderChanged", slider.value)
+        self.videoContainerView.seek(value: Double(slider.value))
+        self.videoContainerView.playVideo()
+    }
+    
+    @objc internal func progressSliderBeginTouch() {
+        self.videoContainerView.pauseVideo()
+    }
+    
+    @objc internal func hideControls() {
+//        delegate?.willHideControls?()
+        UIView.animate(withDuration: 0.3, animations: {
+            self.bottomControlView.alpha = 0.0
+            self.safeAreaBottomView.alpha = 0.0
+        }, completion: { finished in
+//            self.delegate?.didHideControls?()
+        })
+    }
+    
+    @objc internal func toggleControls() {
+        if bottomControlView.alpha == 1.0 {
+            hideControls()
+        } else {
+            controlsToggleWorkItem?.cancel()
+            controlsToggleWorkItem = DispatchWorkItem(block: { [weak self] in
+                self?.hideControls()
+            })
+            
+            showControls()
+            
+            //            if videoPlayerView.status == .playing {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: controlsToggleWorkItem!)
+            //            }
+        }
+    }
+    
+    internal func showControls() {
+//        delegate?.willShowControls?()
+        UIView.animate(withDuration: 0.3, animations: {
+            self.bottomControlView.alpha = 1.0
+            self.safeAreaBottomView.alpha = 1.0
+        }, completion: { finished in
+//            self.delegate?.didShowControls?()
+        })
+    }
+    
+    private func timeFormatted(totalSeconds: UInt) -> String {
+        let seconds = totalSeconds % 60
+        let minutes = (totalSeconds / 60) % 60
+        let hours = totalSeconds / 3600
+
+        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
     }
 
 
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+//    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+//        if let view = touch.view, view.isDescendant(of: self) == true, view != videoPlayerView,
+//            view != videoPlayerControls || touch.location(in: videoPlayerControls).y > videoPlayerControls.bounds.size.height - 50 {
+//            return false
+//        } else {
+//            return true
+//        }
+//    }
 }
 
